281fed2a45b909afac6eedd81aa1f380145cf145
JSONRete <jsonrete@gmail.com>
2022-03-05 01:04:32 -0500

    changed note type from string to text

--- db/migrate/20220305042142_create_tips.rb
+++ db/migrate/20220305042142_create_tips.rb
@@ -1,7 +1,7 @@
 class CreateTips < ActiveRecord::Migration[6.1]
   def change
     create_table :tips do |t|
-      t.string :note
+      t.text :note
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :trick, null: false, foreign_key: true
 
