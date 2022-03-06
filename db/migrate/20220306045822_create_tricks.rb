0954ff50454a42afe1708d1a930c87dd1d02dc70
JSONRete <jsonrete@gmail.com>
2022-03-05 23:58:54 -0500

    generated CreateTricks migration with two belongs_to

---
+++ db/migrate/20220306045822_create_tricks.rb
@@ -0,0 +1,11 @@
+class CreateTricks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tricks do |t|
+      t.string :name
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
