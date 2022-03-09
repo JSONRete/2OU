dc501d692ae7861f201b8411bba0575138dfc49a
JSONRete <jsonrete@gmail.com>
2022-03-08 23:12:30 -0500

    adujusted CreateTricks migration, copied from previous project

--- db/migrate/20220306045822_create_tricks.rb
+++ db/migrate/20220306045822_create_tricks.rb
@@ -1,7 +1,18 @@
 class CreateTricks < ActiveRecord::Migration[6.1]
+  # def change
+  #   create_table :tricks do |t|
+  #     t.string :name
+  #     t.belongs_to :user, null: false, foreign_key: true
+  #     t.belongs_to :category, null: false, foreign_key: true
+
+  #     t.timestamps
+  #   end
+  # end
+  #above is current idea, below is copied from orginal project github
   def change
     create_table :tricks do |t|
-      t.string :name
+      t.string :name, :null => false
+      t.index :name
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :category, null: false, foreign_key: true
 
