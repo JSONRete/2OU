5ac76e015a55e99ba9f485c413347b9e777871fc
JSONRete <jsonrete@gmail.com>
2022-03-09 22:27:10 -0500

    added Catefories controller

---
+++ app/controllers/catefories_controller.rb
@@ -0,0 +1,2 @@
+class CateforiesController < ApplicationController
+end
---
+++ test/controllers/catefories_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class CateforiesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
