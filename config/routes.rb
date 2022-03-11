b26b5d1cab9e77005dc57b555c7d66602c76364b
JSONRete <jsonrete@gmail.com>
2022-03-10 23:05:57 -0500

    plugged in routes from 1st project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,34 @@
 Rails.application.routes.draw do
+  root 'sessions#home'
+
+  get '/auth/google_oauth2/callback' => 'sessions#omniauth'
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
+  delete '/logout' => 'sessions#destroy'
+
+
+  resources :tricks do
+    resources :tips
+  end
+
+  resources :tips
+
+  resources :users do
+    resources :tricks, shallow: true
+  end
+
+  resources :categories, only: [:index, :show]
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# end
