31886e73bcda9126af4fbd31c2b415445d333b8b
JSONRete <jsonrete@gmail.com>
2022-03-24 17:38:37 -1000

    edit test - for amount of commits @once

--- db/migrate/20220306045822_create_tricks.rb
+++ db/migrate/20220306045822_create_tricks.rb
@@ -6,7 +6,6 @@ class CreateTricks < ActiveRecord::Migration[6.1]
       t.index :name
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :category, null: false, foreign_key: true
-
       t.timestamps
     end
   end
